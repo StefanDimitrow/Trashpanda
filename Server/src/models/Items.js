0781b0d4ae5fae3f2579fbd48f7534a9cb8becf1
Stefan Dimitrov <117269448+StefanDimitrow@users.noreply.github.com>
2024-07-11 18:44:46 +0300

    Started making the Backend

---
+++ Server/src/models/Items.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  author: { type: String, required: true },
+
+});
+
+module.exports= mongoose.model('Item', itemSchema)
